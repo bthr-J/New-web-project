b86c79c473db4de1548b578dafacc65b1609147a
bthr-J <jkargo3@gmail.com>
2018-06-29 15:59:34 +0900

    add fizzbuzz file

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(arr) {
+  var inputArr =[];
+  inputArr.push(arr);
+  inputArr.map(Element =>{
+    if(Element % 15 == 0){
+      console.log('FizzBuzz');
+    }else if(Element % 3 == 0){
+      console.log('Fizz');
+    }else if(Element % 5 == 0){
+      console.log('Buzz');
+    }else console.log(Element);
+  });
+}
+
+fizzbuzz(1);
